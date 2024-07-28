a6483763c84534ccb0bb61c736b39b85858c46a7
Gurpreet <dhoat30@gmail.com>
2024-07-28 13:00:02 +1200

    added meta info

--- app/page.js
+++ app/page.js
@@ -3,29 +3,25 @@ import WebsiteInquiryPage from "@/components/Pages/WebsiteInquiryPage/WebsiteInq
 import { getPageData } from '@/utlis/fetchData'
 
 export async function generateMetadata({ params, searchParams }, parent) {
-    // fetch data
-    const data = await getPageData("website-enquiry")
-    // optionally access and extend (rather than replace) parent metadata
-    const previousImages = (await parent).openGraph?.images || []
-    if (data.length > 0) {
-        const seoData = data[0].yoast_head_json
-        return {
+
+
+    return {
+        title: "Request A Free Quote - Treescene NZ Limited",
+        description: "Trust Tree Scene for Safe and Professional Tree Removal, Trimming, and Stump Grinding Services",
+        metadataBase: new URL('https://form.treescene.co.nz'),
+        alternates: {
+            canonical: `https://form.treescene.co.nz`,
+        },
+        openGraph: {
             title: "Request A Free Quote - Treescene NZ Limited",
             description: "Trust Tree Scene for Safe and Professional Tree Removal, Trimming, and Stump Grinding Services",
-            metadataBase: new URL('https://form.treescene.co.nz'),
-            alternates: {
-                canonical: `https://form.treescene.co.nz`,
-            },
-            openGraph: {
-                title: "Request A Free Quote - Treescene NZ Limited",
-                description: "Trust Tree Scene for Safe and Professional Tree Removal, Trimming, and Stump Grinding Services",
-                url: 'https://form.treescene.co.nz',
-                siteName: 'Tree Scene',
+            url: 'https://form.treescene.co.nz',
+            siteName: 'Tree Scene',
 
-                type: 'website',
-            },
-        }
+            type: 'website',
+        },
     }
+
 
 }
 
